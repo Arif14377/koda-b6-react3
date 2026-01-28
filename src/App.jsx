f9b3086e38d95c9aa3f268b16436fa1d6c80f2f1
Arif14377 <arifrahman14377@gmail.com>
2026-01-28 07:45:04 +0700

    feat: create map data from fetch with statis filter keyword

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
 import Input from "./components/Input"
-import React from "react"
+import React, { useState } from "react"
 
 function App() {
   const url = "https://rickandmortyapi.com/api/character"
+
+  const [data, setData] = useState([])
+
   React.useEffect(()=>{
-    async function getData(url) {
+    async function getData() {
       const resp = await fetch(url)
       if(!resp.ok) {
         throw new Error("HTTP Error", resp.status)
       }
       const data = await resp.json()
-      console.log(data)
-      // return data
+      setData(data)
     }
     getData()
-  }, [])
+  }, [url])
+  const keyword = "o"
   return (
     <div>
       <Input id="search" label="Search" name="search" placeholder="Search your character..."/>
+      <div className="flex flex-wrap gap-6 justify-between">
+        {
+          data.results && data.results.filter(r=>r.name.toLowerCase().includes(keyword.toLowerCase())).map((r, idx) =>{
+            return (
+              <div key={idx}>
+                <img src={r.image} alt={r.name} />
+                <h2>{r.name}</h2>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 
